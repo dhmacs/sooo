64704d6c3c00307c34cb243a2d7cb1a64a357424
Macs <massimodemarchi91@gmail.com>
2014-11-26 22:48:14 -0600

    Hello world

---
+++ Sooo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sooo
+//
+//  Created by Massimo De Marchi on 26/11/14.
+//  Copyright (c) 2014 GioMacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sooo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sooo
+//
+//  Created by Massimo De Marchi on 26/11/14.
+//  Copyright (c) 2014 GioMacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
